5e20662a431a5a1f234176006f6706268803feca
EfraRS-dev <eradaa@uninorte.edu.co>
2025-02-11 23:25:12 -0500

    fixes on sh, cpp, node and compose

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -3,6 +3,13 @@
 echo "Lenguaje,Tiempo (ms)" > results.csv
 
 for lang in python java cpp nodejs r; do
+    echo "Limpiando contenedores e imágenes antes de ejecutar $lang..."
+    
+    docker container prune -f  # Borra contenedores detenidos
+    docker image prune -f -a   # Borra imágenes no utilizadas
+    docker volume prune -f     # Borra volúmenes no usados
+    docker system prune -f     # Borra todo lo que no esté en uso
+    
     echo "Ejecutando $lang..."
     
     START_TIME=$(date +%s%N)
@@ -13,6 +20,13 @@ for lang in python java cpp nodejs r; do
     echo "$lang,$EXECUTION_TIME" >> results.csv
     
     echo "$lang completado en $EXECUTION_TIME ms"
+
+    echo "Limpiando después de ejecutar $lang..."
+    docker container prune -f
+    docker image prune -f -a
+    docker volume prune -f
+    docker system prune -f
+
 done
 
 echo "Resultados guardados en results.csv"
